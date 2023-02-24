6464675a52f5f8cd876548bc6bc7a561bcbd10c0
Ramon Tanoeiro <ramontanoeiro@gmail.com>
2023-02-25 10:15:49 +1100

    Refactored instagram_login

--- utils/instagram_login.py
+++ utils/instagram_login.py
@@ -12,30 +12,40 @@ from glob import glob
 from os.path import expanduser
 from platform import system
 from sqlite3 import OperationalError, connect
+import logging
 
 try:
     from instaloader import ConnectionException, Instaloader
-except ModuleNotFoundError:
-    raise SystemExit("Instaloader not found.\n  pip install [--user] instaloader")
+except ModuleNotFoundError as exc:
+    raise SystemExit(
+        "Instaloader not found.\n  pip install [--user] instaloader"
+    ) from exc
+
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s %(levelname)s: %(message)s"
+)
+
 
 class InstagramLogin:
+    """
+    This class will contain all necessary functions to Login on Instagram.
+    Upon importing it, it should already return the cookie file and username
+    """
+
     def __init__(self) -> None:
-
-
-
-    def get_cookiefile(self):
-        default_cookiefile = {
+        self.default_cookiefile = {
             "Windows": "~/AppData/Roaming/Mozilla/Firefox/Profiles/*/cookies.sqlite",
             "Darwin": "~/Library/Application Support/Firefox/Profiles/*/cookies.sqlite",
         }.get(system(), "~/.mozilla/firefox/*/cookies.sqlite")
-        cookiefiles = glob(expanduser(default_cookiefile))
-        if not cookiefiles:
-            raise SystemExit("No Firefox cookies.sqlite file found. Use -c COOKIEFILE.")
-        return cookiefiles[0]
+        self.cookiefiles = glob(expanduser(self.default_cookiefile))
 
+    def get_cookiefile(self):
+        if not self.cookiefiles:
+            raise SystemExit("No Firefox cookies.sqlite file found. Use -c COOKIEFILE.")
+        return self.cookiefiles[0]
 
     def import_session(self, cookiefile, sessionfile):
-        print(f"Using cookies from {cookiefile}.")
+        logging.info("Using cookies from %c", cookiefile)
         conn = connect(f"file:{cookiefile}?immutable=1", uri=True)
         try:
             cookie_data = conn.execute(
@@ -49,13 +59,14 @@ class InstagramLogin:
         instaloader.context._session.cookies.update(cookie_data)
         username = instaloader.test_login()
         if not username:
-            raise SystemExit("Not logged in. Are you logged in successfully in Firefox?")
-        print(f"Imported session cookie for {username}.")
+            raise SystemExit(
+                "Not logged in. Are you logged in successfully in Firefox?"
+            )
+        logging.info("Imported session cookie for %u.", username)
         instaloader.context.username = username
         instaloader.save_session_to_file(sessionfile)
 
         return cookiefile, username
-
 
     def get_cookie_location(self):
         parser = ArgumentParser()
@@ -63,8 +74,10 @@ class InstagramLogin:
         parser.add_argument("-f", "--sessionfile")
         args = parser.parse_args()
         try:
-            cookiefile, username = self.import_session(args.cookiefile or self.get_cookiefile(), args.sessionfile)
-        except (ConnectionException, OperationalError) as exception:
-            raise SystemExit(f"Cookie import failed: {exception}")
+            cookiefile, username = self.import_session(
+                args.cookiefile or self.get_cookiefile(), args.sessionfile
+            )
+        except (ConnectionException, OperationalError) as excecption:
+            raise SystemExit("Cookie import failed") from excecption
 
         return cookiefile, username
