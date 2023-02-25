4fbd883fbcc8a793889dafb407b89f4c66e10afa
Ramon Tanoeiro <ramontanoeiro@gmail.com>
2023-02-25 19:42:46 +1100

    Updated main script based on new json function

--- instagram_scrapper.py
+++ instagram_scrapper.py
@@ -2,5 +2,10 @@
 
 from utils.scrapping_functions import get_profile_json
 
-
-get_profile_json(user_info=["candy"], number_of_posts=50)
+get_profile_json(
+    user_info_source="hashtag",
+    user_info=["cat", "dog", "bike"],
+    number_of_posts=5,
+    number_of_users=10,
+    columns_with_lists=["post_list", "post_date"],
+)
