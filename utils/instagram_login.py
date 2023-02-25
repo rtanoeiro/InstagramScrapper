f478fa476e996bec0fd26f4e2bc71410228e763f
Ramon Tanoeiro <ramontanoeiro@gmail.com>
2023-02-25 13:03:41 +1100

    Fixed login module and removed class as it's not necessary

--- utils/instagram_login.py
+++ utils/instagram_login.py
@@ -6,13 +6,14 @@ the usual instaloader.login function is requiring user input on the browser.
 2. After that you can run instagram_scrapper.py
     and it should get cookie and username from this module.
 """
-# TODO: CONVERT INTO A CLASS
 from argparse import ArgumentParser
 from glob import glob
 from os.path import expanduser
 from platform import system
 from sqlite3 import OperationalError, connect
 import logging
+import sys
+import io
 
 try:
     from instaloader import ConnectionException, Instaloader
@@ -25,85 +26,93 @@ logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s %(levelname)s: %(message)s"
 )
 
-
-class InstagramLogin:
+def get_cookiefile():
     """
-    This class will contain all necessary functions to Login on Instagram.
-    Upon importing it, it should already return the cookie file and username
+    This function will get the cookie file from the login on Mozilla
+
+    Raises:
+        SystemExit: In case there are no cookies, an error is thrown
+
+    Returns:
+        cookiefiles[0]: The path location to the cookie
     """
+    default_cookiefile = {
+        "Windows": "~/AppData/Roaming/Mozilla/Firefox/Profiles/*/cookies.sqlite",
+        "Darwin": "~/Library/Application Support/Firefox/Profiles/*/cookies.sqlite",
+    }.get(system(), "~/.mozilla/firefox/*/cookies.sqlite")
+    cookiefiles = glob(expanduser(default_cookiefile))
+    if not cookiefiles:
+        raise SystemExit("No Firefox cookies.sqlite file found. Use -c COOKIEFILE.")
+    return cookiefiles[0]
 
-    def __init__(self) -> None:
-        self.default_cookiefile = {
-            "Windows": "~/AppData/Roaming/Mozilla/Firefox/Profiles/*/cookies.sqlite",
-            "Darwin": "~/Library/Application Support/Firefox/Profiles/*/cookies.sqlite",
-        }.get(system(), "~/.mozilla/firefox/*/cookies.sqlite")
-        self.cookiefile = glob(expanduser(self.default_cookiefile))[0]
-        if not self.cookiefile:
-            raise SystemExit("No Firefox cookies.sqlite file found. Use -c COOKIEFILE.")
-  
-    def import_session(self, cookiefile: str, sessionfile:str):
-        """
-        This function will be used to grab the username and cookie
-        from the Mozilla Firefox Session
+def import_session(cookiefile: str, sessionfile: str):
+    """
+    This function will be used to grab the username and cookie
+    from the Mozilla Firefox Session
 
-        Args:
-            cookiefile (str): cookie file from get_cookiefile function
-            sessionfile (str): filename where cookie is stored
+    Args:
+        cookiefile (str): cookie file from get_cookiefile function
+        sessionfile (str): filename where cookie is stored
 
-        Raises:
-            SystemExit: In case user is not logged in on Mozilla Firedox,
-            this will thrown an error 
+    Raises:
+        SystemExit: In case user is not logged in on Mozilla Firedox,
+        this will thrown an error
 
-        Returns:
-            cookiefile, username: Returns the cookie file location and username
-            that is logged in to be used on the get_cookie_file_and_username function
-        """
-        logging.info("Using cookies from %s", cookiefile)
-        conn = connect(f"file:{cookiefile}?immutable=1", uri=True)
-        try:
-            cookie_data = conn.execute(
-                "SELECT name, value FROM moz_cookies WHERE baseDomain='instagram.com'"
-            )
-        except OperationalError:
-            cookie_data = conn.execute(
-                "SELECT name, value FROM moz_cookies WHERE host LIKE '%instagram.com'"
-            )
-        instaloader = Instaloader(max_connection_attempts=1)
-        instaloader.context._session.cookies.update(cookie_data)
-        username = instaloader.test_login()
-        if not username:
-            raise SystemExit(
-                "Not logged in. Are you logged in successfully in Firefox?"
-            )
-        logging.info("Imported session cookie for %s.", username)
-        instaloader.context.username = username
-        instaloader.save_session_to_file(sessionfile)
+    Returns:
+        cookiefile, username: Returns the cookie file location and username
+        that is logged in to be used on the get_cookie_file_and_username function
+    """
+    logging.info("Using cookies from %s", cookiefile)
+    conn = connect(f"file:{cookiefile}?immutable=1", uri=True)
+    try:
+        cookie_data = conn.execute(
+            "SELECT name, value FROM moz_cookies WHERE baseDomain='instagram.com'"
+        )
+    except OperationalError:
+        cookie_data = conn.execute(
+            "SELECT name, value FROM moz_cookies WHERE host LIKE '%instagram.com'"
+        )
+    instaloader = Instaloader(max_connection_attempts=1)
+    instaloader.context._session.cookies.update(cookie_data)
+    username = instaloader.test_login()
+    if not username:
+        raise SystemExit(
+            "Not logged in. Are you logged in successfully in Firefox?"
+        )
+    logging.info("Imported session cookie for %s.", username)
+    instaloader.context.username = username
 
-        return cookiefile, username
+    buffer = io.StringIO()
+    sys.stdout = buffer
+    instaloader.save_session_to_file(sessionfile)
+    sys.stdout = sys.__stdout__
+    session_file = buffer.getvalue().replace("Saved session to ", "").replace("\n", "")
 
-    def get_cookie_file_and_username(self):
-        """
-        This function will get the cookie file location
-        and username. These will be used load a Instagram Session.
-        The session will then be used to create the instaloader context,
-        user in many of the library class/functions
+    return username, session_file
 
-        Raises:
-            SystemExit: In case the cookie import fails, an error is raised
+def get_username_and_session_file() -> str:
+    """
+    This function will get the cookie file location
+    and username. These will be used load a Instagram Session.
+    The session will then be used to create the instaloader context,
+    user in many of the library class/functions
 
-        Returns:
-            cookiefile, username: Returns the cookie file location and username
-            that is logged in to be used on the get_cookie_file_and_username function
-        """
-        parser = ArgumentParser()
-        parser.add_argument("-c", "--cookiefile")
-        parser.add_argument("-f", "--sessionfile")
-        args = parser.parse_args()
-        try:
-            cookiefile, username = self.import_session(
-                args.cookiefile or self.cookiefile, args.sessionfile
-            )
-        except (ConnectionException, OperationalError) as excecption:
-            raise SystemExit("Cookie import failed") from excecption
+    Raises:
+        SystemExit: In case the cookie import fails, an error is raised
 
-        return cookiefile, username
+    Returns:
+        cookiefile, username: Returns the cookie file location and username
+        that is logged in to be used on the get_cookie_file_and_username function
+    """
+    parser = ArgumentParser()
+    parser.add_argument("-c", "--cookiefile")
+    parser.add_argument("-f", "--sessionfile")
+    args = parser.parse_args()
+    try:
+        username, session_file = import_session(
+            args.cookiefile or get_cookiefile(), args.sessionfile
+        )
+    except (ConnectionException, OperationalError) as excecption:
+        raise SystemExit("Cookie import failed") from excecption
+
+    return username, session_file
