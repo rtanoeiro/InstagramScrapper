ff11ba0f5a1d31f8e0695c2ff845bef5fdea0100
Ramon Tanoeiro <ramontanoeiro@gmail.com>
2023-02-25 13:21:41 +1100

    Removed class from scrapping function as it was not necessary

--- utils/scrapping_functions.py
+++ utils/scrapping_functions.py
@@ -16,149 +16,138 @@ insta.load_session_from_file(
     filename=session_file,
 )
 
-class ScrappingFunctions:
+
+def _get_usernames_from_file(filename: str) -> list:
+    """This will return an iterable list of usernames that came from a file
+    The file should contain one username per line
+
+    Args:
+        filename (str): filename to be used
+
+    Returns:
+        list: list of usernames containing that word
     """
-    This Class will contain several functions used for scrapping data on Instagram
-    based on the purpose for theses scripts, which is scrapping data from the website
+    user_list = []
+    with open(filename, "r", encoding="UTF-8") as file:
+        lines = file.readlines()
 
-    The main purpose is based on a list of usernames scrape data from whatever is available
-    from that username.
+    user_list.append(lines)
+
+    return user_list
+
+def _get_usernames_from_search(search_words: list) -> list:
+    """This function will return a list of usernames that appear from the search
+
+    Args:
+        search_word (str): Word to use as a search on instagram
+        iterations: each run will get around 15 results,
+        so the number of results will be around iterations *15 results
+
+    Returns:
+        list: list of usernames containing that word
     """
+    user_list = []
+    for search_word in search_words:
+        search_results = TopSearchResults(
+            context=insta.context, searchstring=search_word
+        ).get_prefixed_usernames()
 
-    def __init__(self):
-        pass
-    def _get_usernames_from_file(self, filename: str) -> list:
-        """This will return an iterable list of usernames that came from a file
-        The file should contain one username per line
+        for result in search_results:
+            user_list.append(result)
 
-        Args:
-            filename (str): filename to be used
+    return user_list
 
-        Returns:
-            list: list of usernames containing that word
-        """
-        user_list = []
-        with open(filename, "r", encoding="UTF-8") as file:
-            lines = file.readlines()
+def _get_usernames_from_hashtags(hashtags: list) -> list:
+    """This function will return a list of usernames that appear from the search
 
-        user_list.append(lines)
+    Args:
+        search_word (str): Word to use as a search on instagram
+        iterations: each run will get around 15 results,
+        so the number of results will be around iterations *15 results
 
-        return user_list
+    Returns:
+        list: list of usernames containing that word
+    """
+    ## TODO: Adjust to return profiles associated to hashtag
+    user_list = []
+    for hashtag in hashtags:
+        search_results = Hashtag.from_name(context=insta.context, name=hashtag)
 
-    def _get_usernames_from_search(self, search_words: list) -> list:
-        """This function will return a list of usernames that appear from the search
+    return search_results
 
-        Args:
-            search_word (str): Word to use as a search on instagram
-            iterations: each run will get around 15 results,
-            so the number of results will be around iterations *15 results
+def _get_profile_data(profile: str):
+    """
+    This is a helper function to get profile data
 
-        Returns:
-            list: list of usernames containing that word
-        """
-        user_list = []
-        for search_word in search_words:
-            search_results = TopSearchResults(
-                context=insta.context, searchstring=search_word
-            ).get_prefixed_usernames()
+    Args:
+        profile (str): username string to scrape
+    """
+    return Profile.from_username(context=insta.context, username=profile)
 
-            for result in search_results:
-                user_list.append(result)
+def get_profile_json(
+    user_info: str or list, number_of_posts: int
+) -> pd.DataFrame:
+    """Given an user_info, we return a list of dictionaries
+    containing information from usernames
 
-        return user_list
+    Args:
+        user_info (str or list): If an str is given, we get the user list from a file
+            if a list is given, we get from a search
+        number_of_posts (int): number of posts to retrieve data,
+            otherwise most of the posts will be returned, slowing down performance
 
-    def _get_usernames_from_hashtags(self, hashtags: list) -> list:
-        """This function will return a list of usernames that appear from the search
+    Returns:
+        list[dict]: Data retrieved from instaloader
+    """
+    ## TODO: Add options to get profile data, from hashtag, search and filename
+    user_list, post_list, post_dates = [], [], []
+    if isinstance(user_info, str):
+        user_list = _get_usernames_from_file(filename=user_info)
+    elif isinstance(user_info, list):
+        user_list = _get_usernames_from_search(search_words=user_info)
 
-        Args:
-            search_word (str): Word to use as a search on instagram
-            iterations: each run will get around 15 results,
-            so the number of results will be around iterations *15 results
+    profiles_dict = {
+        "user_id": [],
+        "username": [],
+        "followers": [],
+        "followees": [],
+        "is_business_account": [],
+        "biography": [],
+        "profile_pic_url": [],
+        "post_list": [],
+        "posts_dates": [],
+        "similar_accounts": [],
+    }
 
-        Returns:
-            list: list of usernames containing that word
-        """
-        ## TODO: Adjust to return profiles associated to hashtag
-        user_list = []
-        for hashtag in hashtags:
-            search_results = Hashtag.from_name(context=insta.context, name=hashtag)
+    for i, user in enumerate(user_list):
+        logging.info(
+            "Getting data for %s. It's the user number %i out of %i\n", user, i+1, len(user_list)
+        )
+        profile = _get_profile_data(user)
+        profiles_dict["user_id"].append(profile.userid)
+        profiles_dict["username"].append(profile.username)
+        profiles_dict["followers"].append(profile.followers)
+        profiles_dict["followees"].append(profile.followees)
+        profiles_dict["is_business_account"].append(profile.is_business_account)
+        profiles_dict["biography"].append(profile.biography)
+        profiles_dict["profile_pic_url"].append(profile.profile_pic_url)
+        posts = profile.get_posts()
 
-            for result in search_results:
-                user_list.append(result)
+        for n, post in enumerate(posts):
+            if n == number_of_posts:
+                break
+            post_dates.append(post.date)
+            post_list.append(post.caption)
 
-        return user_list
 
-    def _get_profile_data(self, profile: str):
-        """
-        This is a helper function to get profile data
+        profiles_dict["post_list"].append(post_list)
+        profiles_dict["posts_dates"].append(post_dates)
 
-        Args:
-            profile (str): username string to scrape
-        """
-        return Profile.from_username(context=insta.context, username=profile)
+        similar_accounts = profile.get_similar_accounts()
+        similar_accounts_list = []
+        for account in similar_accounts:
+            similar_accounts_list.append(account.username)
 
-    def get_profile_json(
-        self, user_info: str or list, number_of_posts: int
-    ) -> pd.DataFrame:
-        """Given an user_info, we return a list of dictionaries
-        containing information from usernames
+        profiles_dict["similar_accounts"].append(similar_accounts_list)
 
-        Args:
-            user_info (str or list): If an str is given, we get the user list from a file
-                if a list is given, we get from a search
-            number_of_posts (int): number of posts to retrieve data,
-                otherwise most of the posts will be returned, slowing down performance
-
-        Returns:
-            list[dict]: Data retrieved from instaloader
-        """
-        ## TODO: Add options to get profile data, from hashtag, search and filename
-        user_list, post_list, post_dates = [], [], []
-        if user_info is str:
-            user_list = self._get_usernames_from_file(filename=user_info)
-        elif user_info is list:
-            user_list = self._get_usernames_from_search(search_words=user_info)
-
-        profiles_dict = {
-            "user_id": [],
-            "username": [],
-            "followers": [],
-            "followees": [],
-            "is_business_account": [],
-            "biography": [],
-            "profile_pic_url": [],
-            "post_list": [],
-            "posts_dates": [],
-        }
-        similar_accounts_dict = {
-            "similar_accounts": [],
-        }
-        for i, user in enumerate(user_list):
-            print(
-                f"Getting data for {user}. It's the user number {i+1} out of {len(user_list)}\n"
-            )
-            profile = self._get_profile_data(user)
-            profiles_dict["user_id"].append(profile.userid)
-            profiles_dict["username"].append(profile.username)
-            profiles_dict["followers"].append(profile.followers)
-            profiles_dict["followees"].append(profile.followees)
-            profiles_dict["is_business_account"].append(profile.is_business_account)
-            profiles_dict["biography"].append(profile.biography)
-            profiles_dict["profile_pic_url"].append(profile.profile_pic_url)
-            posts = profile.get_posts()
-
-            for post in posts:
-                post_dates.append(post.date)
-                post_list.append(post.caption)
-
-            profiles_dict["post_list"].append(post_list)
-            profiles_dict["posts_dates"].append(post_dates)
-
-            similar_accounts = profile.get_similar_accounts()
-            similar_accounts_list = []
-            for account in similar_accounts:
-                similar_accounts_list.append(account.username)
-
-            similar_accounts_dict["similar_accounts"].append(similar_accounts_list)
-
-        return pd.DataFrame(profiles_dict), pd.DataFrame(similar_accounts_dict)
+    return pd.DataFrame(profiles_dict)
